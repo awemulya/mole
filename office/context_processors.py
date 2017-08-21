96564c3a330c3d04d3e4938f7a6b6ab3c283b9f9
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-08-21 14:50:07 +0545

    deleted

--- office/context_processors.py
+++
@@ -1,8 +0,0 @@
-from office.models import District, Office
-
-
-def dcms_data(request):
-    return {
-        'districts': District.objects.all(),
-        'projectlinks': Office.objects.filter(is_project=True),
-    }
