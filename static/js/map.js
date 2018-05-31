26234bda7317b0b3daea812407252f76fe1f8739
awemulya <awemulya@gmail.com>
2018-05-31 14:16:10 +0545

    map clean

--- static/js/map.js
+++ static/js/map.js
@@ -39,7 +39,7 @@ var baseLayers = {
         layerswitcher = L.control.layers(baseLayers, {}, {collapsed: true}).addTo(map);
 
         var icon = L.icon({
-          iconUrl: 'https://png.pngtree.com/element_origin_min_pic/16/12/02/51e6452ca365f618ab4b723c7aa18be9.jpg',
+          iconUrl: 'https://files.slack.com/files-pri/T1QG0R92R-FAX1UMY8Y/marker-icon.png',
           iconSize: [32, 37],
           iconAnchor: [16, 37],
           popupAnchor: [0, -37]
@@ -48,13 +48,13 @@ var baseLayers = {
                 L.marker([parseFloat(lat), parseFloat(lon)],{icon:icon}).bindPopup(name).addTo(map);
         }else{
 
-            L.marker([27.6692636, 85.0326804],{icon:icon}).bindPopup("बैदेशिक रोजगार बिभाग ").addTo(map);
-            L.marker([27.696446, 85.1221903],{icon:icon}).bindPopup("श्रम विभाग").addTo(map);
-            L.marker([27.6931977, 85.2242567],{icon:icon}).bindPopup("वैदेशिक रोजगार प्रवर्द्धन वोर्ड").addTo(map);
-            L.marker([27.6482818, 85.4050494],{icon:icon}).bindPopup("व्यवसायिक तथा सीप विकास तालिम केन्द्र").addTo(map);
-            L.marker([27.6867262, 85.6365863],{icon:icon}).bindPopup("व्यवसायजन्य सुरक्षा तथा स्वास्थ्य सम्बन्धी आयोजना").addTo(map);
-            L.marker([27.6931977, 85.7242567],{icon:icon}).bindPopup("ई. पि. एस. कोरिया शाखा").addTo(map);
-            L.marker([27.6482818, 85.9050494],{icon:icon}).bindPopup("सामाजिक सुरक्षा कोष").addTo(map);
-            L.marker([27.6867262, 85.5365863],{icon:icon}).bindPopup("वैदेशिक रोजगार न्यायधिकरण").addTo(map);
+            L.marker([27.687096, 85.327176],{icon:icon}).bindPopup("बैदेशिक रोजगार बिभाग ").addTo(map);
+            L.marker([27.686727, 85.337680],{icon:icon}).bindPopup("श्रम विभाग").addTo(map);
+            L.marker([ 27.692456, 85.335741],{icon:icon}).bindPopup("वैदेशिक रोजगार प्रवर्द्धन वोर्ड").addTo(map);
+            L.marker([ 27.646982, 85.308698],{icon:icon}).bindPopup("व्यवसायिक तथा सीप विकास तालिम केन्द्र").addTo(map);
+            L.marker([27.647100, 85.308526],{icon:icon}).bindPopup("व्यवसायजन्य सुरक्षा तथा स्वास्थ्य सम्बन्धी आयोजना").addTo(map);
+            L.marker([27.669276, 85.334866],{icon:icon}).bindPopup("ई. पि. एस. कोरिया शाखा").addTo(map);
+            L.marker([27.693550,85.325032],{icon:icon}).bindPopup("सामाजिक सुरक्षा कोष").addTo(map);
+            L.marker([ 27.693610,  85.324807],{icon:icon}).bindPopup("वैदेशिक रोजगार न्यायधिकरण").addTo(map);
         }
 
