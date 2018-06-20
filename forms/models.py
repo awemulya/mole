5b5ee40689a1b99771f1967cfef44e7b4f3c1663
JoPuskar <puskarjoshi22@gmail.com>
2018-06-20 14:48:26 +0545

    indentations to spaces

--- forms/models.py
+++ forms/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class DarbandiVivaran(models.Model):
     pad_naam = models.CharField(max_length=50)
     shreni = models.CharField(max_length=50)
@@ -23,50 +24,45 @@ class DarbandiVivaran(models.Model):
 
 
 class BhautikPurwadhar(models.Model):
-	bibaran = models.CharField(max_length=250)
-	parimand = models.IntegerField(default=0)
-	chalu_halat = models.IntegerField(default=0)
-	bigreko = models.IntegerField(default=0)
-	marmat = models.IntegerField(default=0)
-	kaifiyat = models.CharField(max_length=250, null=True, blank=True)
-	office = models.CharField(max_length=50)
+    bibaran = models.CharField(max_length=250)
+    parimand = models.IntegerField(default=0)
+    chalu_halat = models.IntegerField(default=0)
+    bigreko = models.IntegerField(default=0)
+    marmat = models.IntegerField(default=0)
+    kaifiyat = models.CharField(max_length=250, null=True, blank=True)
+    office = models.CharField(max_length=50)
 
-	def __str__(self):
-		return self.bibaran
+    def __str__(self):
+        return self.bibaran
 
 
 class AamdaniKharcha(models.Model):
-	sirshak = models.CharField(max_length=50)
-	biniyojit = models.IntegerField(default=0)
-	kharcha = models.IntegerField(default=0)
-	prapta_rakam = models.IntegerField(default=0)
-	kharcha_bibaran = models.IntegerField(default=0)
-	kaifiyat = models.CharField(max_length=250, null=True, blank=True)
+    sirshak = models.CharField(max_length=50)
+    biniyojit = models.IntegerField(default=0)
+    kharcha = models.IntegerField(default=0)
+    prapta_rakam = models.IntegerField(default=0)
+    kharcha_bibaran = models.IntegerField(default=0)
+    kaifiyat = models.CharField(max_length=250, null=True, blank=True)
 
-	def __str__(self):
-		return self.sirshak
+    def __str__(self):
+        return self.sirshak
 
 
 class RajaswaVivaran(models.Model):
-	rajaswo_sirshak = models.CharField(max_length=50)
-	rakam = models.IntegerField(default=0)
+    rajaswo_sirshak = models.CharField(max_length=50)
+    rakam = models.IntegerField(default=0)
 
-	def __str__(self):
-		return self.rajaswo_sirshak
+    def __str__(self):
+        return self.rajaswo_sirshak
 
 
 class BerujuVivaran(models.Model):
-	beruju_prakar = models.CharField(max_length=250)
-	beruju_rakam = models.IntegerField(default=0)
-	phachheut_rakam = models.IntegerField(default=0)
-	phachheut_banki_rakam = models.IntegerField(default=0)
-	samparichhyan_rakam = models.IntegerField(default=0)
-	kaifiyat = models.CharField(max_length=250, null=True, blank=True)
+    beruju_prakar = models.CharField(max_length=250)
+    beruju_rakam = models.IntegerField(default=0)
+    phachheut_rakam = models.IntegerField(default=0)
+    phachheut_banki_rakam = models.IntegerField(default=0)
+    samparichhyan_rakam = models.IntegerField(default=0)
+    kaifiyat = models.CharField(max_length=250, null=True, blank=True)
 
-	def __str__(self):
-		return self.beruju_prakar
-
-
-
-
-		
+    def __str__(self):
+        return self.beruju_prakar
