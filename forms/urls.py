37ac0bc42626195af6786ae886f3a3da74b92278
JoPuskar <puskarjoshi22@gmail.com>
2018-06-21 12:19:51 +0545

    Darbandi table crud

--- forms/urls.py
+++ forms/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 
+from forms.views import DarbandiVivaranCreateView, DarbandiVivaranListView, DarbandiVivaranUpdateView
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^darbandi-vivaran-create/$', DarbandiVivaranCreateView.as_view(), name='darbandi-create'),
+    url(r'^darbandi-vivaran-list/$', DarbandiVivaranListView.as_view(), name='darbandi-list'),
+    url(r'^darbandi-vivaran-update/(?P<pk>\d+)/$', DarbandiVivaranUpdateView.as_view(), name='darbandi-update'),
 ]
--- forms/views.py
+++ forms/views.py
@@ -1,6 +1,38 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+
+from forms.models import DarbandiVivaran
 
 
 def index(request):
     return HttpResponse("Hello, world.")
+
+
+class DarbandiVivaranCreateView(CreateView):
+    model = DarbandiVivaran
+    template_name = "forms/darbarndi_vivaran_create.html"
+    success_url = reverse_lazy("forms:darbandi-list")
+    fields = '__all__'
+
+
+class DarbandiVivaranListView(ListView):
+    model = DarbandiVivaran
+    paginate_by = 10
+    template_name = "forms/darbarndi_vivaran_list.html"
+
+
+class DarbandiVivaranDetailView(DetailView):
+    model = DarbandiVivaran
+    template_name = "forms/darbarndi_vivaran_detail.html"
+
+
+class DarbandiVivaranUpdateView(UpdateView):
+    model = DarbandiVivaran
+    fields = '__all__'
+    template_name = "forms/darbarndi_vivaran_create.html"
+    success_url = reverse_lazy("forms:darbandi-list")
+
