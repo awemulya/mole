3d45e3da875afd4947c1defdf6b17b019d23379c
Pramod Tiwari <you@tpromode@gmai.com>
2018-06-21 14:53:22 +0545

    developing crud of Bhautikpurbadhar,Rajaswa_Bibaran

--- forms/urls.py
+++ forms/urls.py
@@ -1,11 +1,20 @@
 from django.conf.urls import url
 
-from forms.views import DarbandiVivaranCreateView, DarbandiVivaranListView, DarbandiVivaranUpdateView
+from forms.views import DarbandiVivaranCreateView, DarbandiVivaranListView,\
+ DarbandiVivaranUpdateView, BhautikPurwadharCreateView, BhautikPurwadharListView,\
+  BhautikPurwadharUpdateView, RajaswaVivaranCreateView, RajaswaVivaranListView,\
+   RajaswaVivaranUpdateView
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^darbandi-vivaran-create/$', DarbandiVivaranCreateView.as_view(), name='darbandi-create'),
-    url(r'^darbandi-vivaran-list/$', DarbandiVivaranListView.as_view(), name='darbandi-list'),
-    url(r'^darbandi-vivaran-update/(?P<pk>\d+)/$', DarbandiVivaranUpdateView.as_view(), name='darbandi-update'),
+    url(r'^darbandi-vivaran-create/$', DarbandiVivaranCreateView.as_view(), name='darbandi_create'),
+    url(r'^darbandi-vivaran-list/$', DarbandiVivaranListView.as_view(), name='darbandi_list'),
+    url(r'^darbandi-vivaran-update/(?P<pk>\d+)/$', DarbandiVivaranUpdateView.as_view(), name='darbandi_update'),
+    url(r'^bhautik-purwadhar-create/$', BhautikPurwadharCreateView.as_view(), name='bhautik_create'),
+    url(r'^bhautik-purwadhar-list/$', BhautikPurwadharListView.as_view(), name='bhautik_list'),
+    url(r'^bhautik-purwadhar-update/(?P<pk>\d+)/$', BhautikPurwadharUpdateView.as_view(), name='bhautik_update'),
+    url(r'^rajaswa-vivaran-create/$', RajaswaVivaranCreateView.as_view(), name='rajaswa_create'),
+    url(r'^rajaswa-vivaran-list/$', RajaswaVivaranListView.as_view(), name='rajaswa_list'),
+    url(r'^rajaswa-vivaran-update/(?P<pk>\d+)/$', RajaswaVivaranUpdateView.as_view(), name='rajaswa_update'),
 ]
--- forms/views.py
+++ forms/views.py
@@ -5,7 +5,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 
-from forms.models import DarbandiVivaran
+from forms.models import DarbandiVivaran, BhautikPurwadhar, RajaswaVivaran
 
 
 def index(request):
@@ -15,7 +15,7 @@ def index(request):
 class DarbandiVivaranCreateView(CreateView):
     model = DarbandiVivaran
     template_name = "forms/darbarndi_vivaran_create.html"
-    success_url = reverse_lazy("forms:darbandi-list")
+    success_url = reverse_lazy("forms:darbandi_list")
     fields = '__all__'
 
 
@@ -34,5 +34,55 @@ class DarbandiVivaranUpdateView(UpdateView):
     model = DarbandiVivaran
     fields = '__all__'
     template_name = "forms/darbarndi_vivaran_create.html"
-    success_url = reverse_lazy("forms:darbandi-list")
+    success_url = reverse_lazy("forms:darbandi_list")
 
+
+class BhautikPurwadharCreateView(CreateView):
+    model = BhautikPurwadhar
+    template_name = "forms/bhautik_purwadhar_create.html"
+    success_url = reverse_lazy("forms:bhautik_create")
+    fields = '__all__'
+
+
+class BhautikPurwadharListView(ListView):
+    model = BhautikPurwadhar
+    paginate_by = 10
+    template_name = "forms/bhautik_purwadhar_list.html"
+
+
+class BhautikPurwadharDetailView(DetailView):
+    model = BhautikPurwadhar
+    template_name = "forms/bhautik_purwadhar_detail.html"
+
+
+class BhautikPurwadharUpdateView(UpdateView):
+    model = BhautikPurwadhar
+    fields = '__all__'
+    template_name = "forms/bhautik_purwadhar_create.html"
+    success_url = reverse_lazy("forms:bhautik_list")
+
+
+
+class RajaswaVivaranCreateView(CreateView):
+    model = RajaswaVivaran
+    template_name = "forms/rajaswa_vivaran_create.html"
+    success_url = reverse_lazy("forms:rajaswa_create")
+    fields = '__all__'
+
+
+class RajaswaVivaranListView(ListView):
+    model = RajaswaVivaran
+    paginate_by = 10
+    template_name = "forms/rajaswa_vivaran_list.html"
+
+
+class RajaswaVivaranDetailView(DetailView):
+    model = RajaswaVivaran
+    template_name = "forms/rajaswa_vivaran_detail.html"
+
+
+class RajaswaVivaranUpdateView(UpdateView):
+    model = RajaswaVivaran
+    fields = '__all__'
+    template_name = "forms/rajaswa_vivaran_create.html"
+    success_url = reverse_lazy("forms:rajaswa_list")
